d6589dbb003d66ab93606705b5262c9cf240cd73
BilalAwan <Bilalhassanawan40@gmail.com>
2025-03-03 06:19:01 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
 }
+
+function previewImage(event) {
+    const reader = new FileReader();
+    reader.onload = function() {
+        const profilePic = document.getElementById('profilePic');
+        profilePic.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+}
